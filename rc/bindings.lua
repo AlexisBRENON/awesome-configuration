98ae157069338ea66f8daf8083c7d40d0f7f447e
Alexis BRENON <brenon.alexis@gmail.com>
2014-05-11 12:16:51 +0200

    Bindings: Remove some unused key bindings and add mines.

--- rc/bindings.lua
+++ rc/bindings.lua
@@ -112,142 +112,192 @@ local display_nmaster_ncol =
 config.keys.global = awful.util.table.join(
   keydoc.group("Focus"),
   awful.key({ modkey,           }, "j",
-	     function ()
-		awful.client.focus.byidx( 1)
-		if client.focus then
-		   client.focus:raise()
-		end
-	     end,
-	     "Focus next window"),
-   awful.key({ modkey,           }, "k",
-	     function ()
-		awful.client.focus.byidx(-1)
-		if client.focus then
-		   client.focus:raise()
-		end
-	     end,
-	     "Focus previous window"),
-   awful.key({ modkey,           }, "Tab",
-	     function ()
-		awful.client.focus.history.previous()
-		if client.focus then
-		   client.focus:raise()
-		end
-	     end,
-	     "Focus previously focused window"),
-   awful.key({ modkey, "Control" }, "j", function ()
-		screen_focus( 1)
-					 end,
-	     "Jump to next screen"),
-   awful.key({ modkey, "Control" }, "k", function ()
-		screen_focus(-1)
-					 end),
+    function ()
+		  awful.client.focus.byidx( 1)
+		  if client.focus then
+		    client.focus:raise()
+		  end
+    end,
+    "Focus next window"),
+  awful.key({ modkey,           }, "k",
+    function ()
+      awful.client.focus.byidx(-1)
+		  if client.focus then
+        client.focus:raise()
+      end
+    end,
+    "Focus previous window"),
+  awful.key({ modkey,           }, "Tab",
+    function ()
+		  awful.client.focus.history.previous()
+      if client.focus then
+        client.focus:raise()
+      end
+    end,
+    "Focus previously focused window"),
+  awful.key({ modkey, "Control" }, "j",
+    function ()
+      screen_focus( 1)
+    end,
+    "Jump to next screen"),
+  awful.key({ modkey, "Control" }, "k",
+    function ()
+		  screen_focus(-1)
+    end,
+    "Jump to previous screen"),
 
-   keydoc.group("Layout manipulation"),
-   awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end,
-	     "Increase master-width factor"),
-   awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end,
-	     "Decrease master-width factor"),
-   awful.key({ modkey, "Shift"   }, "l",     function ()
-                awful.tag.incnmaster(1)
-                display_nmaster_ncol()
-                                             end,
-	     "Increase number of masters"),
-   awful.key({ modkey, "Shift"   }, "h",     function ()
-                awful.tag.incnmaster(-1)
-                display_nmaster_ncol()
-                                             end,
-	     "Decrease number of masters"),
-   awful.key({ modkey, "Control" }, "l",     function ()
-                awful.tag.incncol(1)
-                display_nmaster_ncol()
-                                             end,
-	     "Increase number of columns"),
-   awful.key({ modkey, "Control" }, "h",     function ()
-                awful.tag.incncol(-1)
-                display_nmaster_ncol()
-                                             end,
-	     "Decrease number of columns"),
-   awful.key({ modkey,           }, "space", function () awful.layout.inc(config.layouts,  1) end,
-	     "Next layout"),
-   awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(config.layouts, -1) end,
-	     "Previous layout"),
-   awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
-	     "Swap with next window"),
-   awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
-	     "Swap with previous window"),
-
-   keydoc.group("Misc"),
-
-   -- Spawn a terminal
-   awful.key({ modkey,           }, "Return", function () awful.util.spawn(config.terminal) end,
-	     "Spawn a terminal"),
+  keydoc.group("Layout manipulation"),
+  awful.key({ modkey,           }, "l",
+    function ()
+      awful.tag.incmwfact( 0.05)
+    end,
+    "Increase master-width factor"),
+  awful.key({ modkey,           }, "h",
+    function ()
+      awful.tag.incmwfact(-0.05)
+    end,
+    "Decrease master-width factor"),
+  awful.key({ modkey, "Shift"   }, "l",
+    function ()
+      awful.tag.incnmaster(1)
+      display_nmaster_ncol()
+    end,
+    "Increase number of masters"),
+  awful.key({ modkey, "Shift"   }, "h",
+    function ()
+      awful.tag.incnmaster(-1)
+      display_nmaster_ncol()
+    end,
+    "Decrease number of masters"),
+  awful.key({ modkey, "Control" }, "l",
+    function ()
+      awful.tag.incncol(1)
+      display_nmaster_ncol()
+    end,
+    "Increase number of columns"),
+  awful.key({ modkey, "Control" }, "h",
+    function ()
+      awful.tag.incncol(-1)
+      display_nmaster_ncol()
+    end,
+	  "Decrease number of columns"),
+  awful.key({ modkey,           }, "Up",
+    function ()
+      awful.layout.inc(config.layouts,  1)
+    end,
+    "Next layout"),
+  awful.key({ modkey,          }, "Down",
+    function ()
+      awful.layout.inc(config.layouts, -1)
+    end,
+    "Previous layout"),
+  awful.key({ modkey, "Shift"   }, "j",
+    function ()
+      awful.client.swap.byidx(  1)
+    end,
+    "Swap with next window"),
+  awful.key({ modkey, "Shift"   }, "k",
+    function ()
+      awful.client.swap.byidx( -1)
+    end,
+    "Swap with previous window"),
+  
+  keydoc.group("Misc"),
+  awful.key({ modkey,           }, "Return",
+    function ()
+      awful.util.spawn(config.terminal)
+    end,
+    "Spawn a terminal"),
+  awful.key({ modkey,           }, "o",
+    function ()
+      awful.util.spawn(config.browser)
+    end,
+    "Spawn a web browser"),
+  awful.key({ modkey,           }, "space",
+    function ()
+      awful.util.spawn(config.filebrowser)
+    end,
+    "Spawn a filebrowser"),
 
    -- Screenshot
-   awful.key({}, "Print", function() screenshot("root") end),
-   awful.key({ modkey, "Shift" }, "Print", screenshot),
+  awful.key({}, "Print",
+    function()
+      screenshot("root")
+    end),
+  awful.key({ "Shift" }, "Print",
+    screenshot),
 
-   -- Restart awesome
-   awful.key({ modkey, "Control" }, "r", awesome.restart),
+  -- Restart awesome
+  awful.key({ modkey, "Control" }, "r", awesome.restart),
+  awful.key({ modkey, "Control" }, "q", awesome.quit),
 
-   -- Multimedia keys
-   awful.key({ }, "XF86MonBrightnessUp",   brightness.increase),
-   awful.key({ }, "XF86MonBrightnessDown", brightness.decrease),
-   awful.key({ }, "XF86AudioRaiseVolume", volume.increase),
-   awful.key({ }, "XF86AudioLowerVolume", volume.decrease),
-   awful.key({ }, "XF86AudioMute",        volume.toggle),
+  -- Multimedia keys
+  awful.key({ }, "XF86MonBrightnessUp",   brightness.increase),
+  awful.key({ }, "XF86MonBrightnessDown", brightness.decrease),
+  awful.key({ }, "XF86AudioRaiseVolume", volume.increase),
+  awful.key({ }, "XF86AudioLowerVolume", volume.decrease),
+  awful.key({ }, "XF86AudioMute",        volume.toggle),
 
-   awful.key({ }, "XF86AudioPlay",        music.playpause),
-   awful.key({ }, "XF86AudioPause",       music.pause),
-   awful.key({ }, "XF86AudioStop",        music.stop),
-   awful.key({ }, "XF86AudioNext",        music.next),
-   awful.key({ }, "XF86AudioPrev",        music.previous),
-
-   -- Help
-   awful.key({ modkey, }, "F1", keydoc.display)
+  -- Help
+  awful.key({ modkey, }, "F1", keydoc.display)
 )
 
 config.keys.client = awful.util.table.join(
-   keydoc.group("Window-specific bindings"),
-   awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end,
-	     "Fullscreen"),
-   awful.key({ modkey,           }, "x",      function (c) c:kill()                         end,
-	     "Close"),
-   awful.key({ modkey,           }, "o",
-             function (c)
-                if screen.count() == 1 then return nil end
-                local s = awful.util.cycle(screen.count(), c.screen + 1)
-                if awful.tag.selected(s) then
-                   c.screen = s
-                   client.focus = c
-                   c:raise()
-                end
-             end, "Move to the other screen"),
-   awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle, "Toggle floating"),
-   awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-	     "Switch with master window"),
-   awful.key({ modkey,           }, "t",      function (c) c:raise()            end,
-	     "Raise window"),
-   awful.key({ modkey,           }, "s",      function (c) c.sticky = not c.sticky end,
-	     "Stick window"),
-   awful.key({ modkey,           }, "i",      dbg,
-	     "Get client-related information"),
-   awful.key({ modkey,           }, "m",
-	     function (c)
-		c.maximized_horizontal = not c.maximized_horizontal
-		c.maximized_vertical   = not c.maximized_vertical
-                c:raise()
-	     end,
-	     "Maximize"),
-
-
-   -- Screenshot
-   awful.key({ modkey }, "Print", screenshot, "Screenshot")
+  keydoc.group("Window-specific bindings"),
+  awful.key({ modkey,           }, "f",
+    function (c)
+      c.fullscreen = not c.fullscreen
+    end,
+    "Fullscreen"),
+  awful.key({ modkey,           }, "x",
+    function (c)
+      c:kill()
+    end,
+    "Close"),
+  awful.key({ modkey,           }, "o",
+    function (c)
+      if screen.count() == 1 then return nil end
+      local s = awful.util.cycle(screen.count(), c.screen + 1)
+      if awful.tag.selected(s) then
+         c.screen = s
+         client.focus = c
+         c:raise()
+      end
+    end,
+    "Move to the other screen"),
+  awful.key({ modkey, "Control" }, "space",
+    awful.client.floating.toggle,
+    "Toggle floating"),
+  awful.key({ modkey, "Control" }, "Return",
+    function (c)
+      c:swap(awful.client.getmaster())
+    end,
+    "Switch with master window"),
+  awful.key({ modkey,           }, "s",
+    function (c)
+      c.sticky = not c.sticky
+    end,
+    "Stick window"),
+  awful.key({ modkey,           }, "i",
+    dbg,
+    "Get client-related information"),
+  awful.key({ modkey,           }, "m",
+    function (c)
+		  c.maximized_horizontal = not c.maximized_horizontal
+		  c.maximized_vertical   = not c.maximized_vertical
+      c:raise()
+    end,
+    "Maximize")
 )
 
 keydoc.group("Misc")
 
 config.mouse.client = awful.util.table.join(
-   awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-   awful.button({ modkey }, 1, awful.mouse.client.move),
-   awful.button({ modkey }, 3, awful.mouse.client.resize))
+  awful.button({ }, 1,
+    function (c)
+      client.focus = c
+      c:raise()
+    end),
+  awful.button({ modkey }, 1, awful.mouse.client.move),
+  awful.button({ modkey }, 3, awful.mouse.client.resize)
+)
