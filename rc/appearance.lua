e749300d0ea1be0398a31d4be7ff448fcc099a52
Alexis BRENON <brenon.alexis@gmail.com>
2014-05-19 21:20:13 +0200

    Revert "Wallpaper: Change how to set the wallpaper to gears module"
    This reverts commit 40c7458aa8cdcdfc5e9b1ee63b64ba2e1f195c84.

--- rc/appearance.lua
+++ rc/appearance.lua
@@ -1,10 +1,5 @@
-gears = require("gears")
-
 -- Theme
 beautiful.init(awful.util.getdir("config") .. "/rc/theme.lua")
-for s = 1, screen.count() do
-	gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-end
 
 -- Set cursor theme
 os.execute("xsetroot -cursor_name left_ptr")
