20db462b4e7403efbedb27018946b65c534a480e
Alexis BRENON <brenon.alexis@gmail.com>
2014-05-25 21:56:52 +0200

    Theme: clean the theme.lua file and group all images required for theme in theme dir. Plus some other minor fixes.

--- mod/appearance.lua
+++ mod/appearance.lua
@@ -1,4 +1,10 @@
+require("beautiful")
+require("awful")
+
 -- Load theme
 beautiful.init(awful.util.getdir("config") .. "/theme/theme.lua")
+-- Link it to awful
+awful.beautiful.register(beautiful)
+
 -- Set cursor theme
 os.execute("xsetroot -cursor_name left_ptr")
