9207b0e3315f87b4c16257d67c9216924c805cf6
Alexis BRENON <brenon.alexis@gmail.com>
2014-06-15 22:00:18 +0200

    Tags : Reduce tag and rules definitions

--- mod/rules.lua
+++ mod/rules.lua
@@ -3,47 +3,9 @@ local shifty = require("shifty")
 shifty.config.apps = {
     {
         match = {
-            config.application.web_browser.class
-        },
-        tag = "🌐",
-    },
-    {
-        match = {
             config.application.file_browser.class
         },
-        tag = "📁",
-    },
-    {
-        match = {
-            "Gimp.*"
-        },
-        tag = "gimp",
-    },
-    {
-        match = {
-            "Vlc"
-        },
-        tag = "vlc",
-    },
-    {
-        match = {
-            "Subl"
-        },
-        tag = "subl",
-    },
-    {
-        match = {
-            "libreoffice.*"
-        },
-        tag = "office",
-    },
-    {
-        match = {
-            "_NET_WM_WINDOW_TYPE_DIALOG",
-            "_NET_WM_WINDOW_TYPE_SPLASH",
-        },
-        float = true,
-        intrusive = true,
+        tag = "files",
     },
     {
         match = {""},
--- mod/tags.lua
+++ mod/tags.lua
@@ -4,31 +4,20 @@ local awful = require("awful")
 
 
 shifty.config.tags = {}
-shifty.config.tags["🌐"] = {
-    index = 1,
-    exclusive = true,
-    spawn = config.application.web_browser.bin,
+shifty.config.tags["1"] = {
+    position = 1,
+    init = true,
 }
 
-shifty.config.tags["📁"] = {
-    index = 2,
-    exclusive = true,
-    spawn = config.application.file_browser.bin,
+shifty.config.tags["file"] = {
+    position = 2,
 }
 
-shifty.config.tags = {
-    gimp = {
-        exclusive = true,
-        icon_only = true,
-        icon = beautiful.taglist_image
-    },
-    vlc = {
-        exclusive = true,
-        icon_only = true,
-        icon = beautiful.taglist_video
-    }
-    -- TODO : to be continued
+shifty.config.tags["3"] = {
+    position = 3,
+    init = true,
 }
+
 
 shifty.config.defaults = {
     layout = awful.layout.layouts[1],
