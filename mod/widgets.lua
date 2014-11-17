24319d61a647f441cbdef3d862f799f80eda75bb
Alexis BRENON <brenon.alexis@gmail.com>
2014-11-17 07:51:27 +0100

    Modify architecture to allow access to all widgets from anywhere Prepare for automatic configuration generation (in project)

--- mod/widgets.lua
+++ mod/widgets.lua
@@ -4,29 +4,75 @@ local vicious = require("vicious")
 local awful = require("awful")
 local naughty = require("naughty")
 local volume = require("mod/volume")
+local gears = require("gears")
 
-config.widget = {}
+config.widgets = {
+    date_time = {
+        widget = nil,
+        update_time = 61,
+        format = "%a %d/%m, %H:%M",
+    },
+    battery = {
+        widget = nil,
+        update_time = 59,
+        device = "BAT0",
+    },
+    volume = {
+        widget = nil,
+        update_time = 3631,
+        device = "IEC958",
+    },
+    separator = {
+        widget = nil,
+        text = " ",
+    },
+    systray = {
+        widget = nil,
+    },
+    wiboxes = {
+        top = {
+            widget = {}, -- One widget per screen
+            size = 22,
+            bg = gears.color.create_pattern({
+                type="linear",
+                from={0,0},
+                to={0,22},
+                stops={
+                    {1, beautiful.bg_normal .. "11"},
+                    {0, beautiful.bg_normal .. "CC"},
+                }
+            }),
+        }
+    },
+    promptbox = {
+        widget = {}, -- One widget per screen
+    },
+    taglist = {
+        widget = {}, -- One per sreen
+        buttons = awful.util.table.join(
+            awful.button({ }, 1, awful.tag.viewonly),
+            awful.button({ modkey }, 1, awful.client.movetotag),
+            awful.button({ }, 3, awful.tag.viewtoggle))
+    },
+    layoutbox = {
+        widget = {}, -- One per screen
+    },
+}
 
 
 -- Separators
-local sep_top_bottom    = wibox.widget.imagebox()
-local sep_bottom_top    = wibox.widget.imagebox()
-local sep_blank         = wibox.widget.textbox()
-sep_top_bottom:set_image(beautiful.icons .. "/widgets/topBottom.png")
-sep_bottom_top:set_image(beautiful.icons .. "/widgets/bottomTop.png")
-sep_blank:set_text(" ")
+config.widgets.separator.widget = wibox.widget.textbox()
+config.widgets.separator.widget:set_text(config.widgets.separator.text)
 
 
 -- Date
-local datewidget = wibox.widget.textbox()
-local dateformat = "%a %d/%m, %H:%M"
+config.widgets.date_time.widget = wibox.widget.textbox()
 vicious.register(
-    datewidget,
+    config.widgets.date_time.widget,
     vicious.widgets.date,
-    dateformat,
-    61
+    config.widgets.date_time.format,
+    config.widgets.date_time.update_time
 )
-config.widget.date_widget = datewidget
 -- Add an icon ??
 -- Define funtions to display/hide calendar
 local calendar = (
@@ -81,9 +127,9 @@ local calendar = (
 )()
 
 -- Display calendar when entering in date widget
-datewidget:connect_signal("mouse::enter", function() calendar.display(0) end)
-datewidget:connect_signal("mouse::leave", calendar.hide)
-datewidget:buttons(
+config.widgets.date_time.widget:connect_signal("mouse::enter", function() calendar.display(0) end)
+config.widgets.date_time.widget:connect_signal("mouse::leave", calendar.hide)
+config.widgets.date_time.widget:buttons(
     awful.util.table.join(
         awful.button({ }, 3, function() calendar.display(-1) end),
         awful.button({ }, 1, function() calendar.display(1) end)
@@ -93,10 +139,9 @@ datewidget:buttons(
 
 -- Battery
 -- TODO : Add a progress bar as battery charge
-local batwidget = {}
-batwidget.widget = wibox.widget.textbox()
+config.widgets.battery.widget = wibox.widget.textbox()
 vicious.register(
-    batwidget.widget,
+    config.widgets.battery.widget,
     vicious.widgets.bat,
     function (widget, args)
         local state = args[1]
@@ -106,30 +151,28 @@ vicious.register(
         if current < 10 and state == "-" then
             -- Maybe we want to display a small warning?
             if current ~= batwidget.lastwarn then
-                batwidget.notification_id = naughty.notify({
+                config.widgets.battery.notification_id = naughty.notify({
                     title = "Batterie : niveau bas!",
                     preset = naughty.config.presets.critical,
                     timeout = 20,
                     text = args[3] .. " restante(s).",
                     icon = awful.util.geticonpath("battery-caution"),
-                    replaces_id = batwidget.notification_id
+                    replaces_id = config.widgets.battery.notification_id
                 }).id
-                batwidget.lastwarn = current
+                config.widgets.battery.lastwarn = current
             end
         end
         -- TODO : hibernate computer if current is critical
         return args[1] .. current .. "%"
     end,
-    59,
-    "BAT0"
+    config.widgets.battery.update_time,
+    config.widgets.battery.device
 )
-config.widget.bat_widget = batwidget.widget
-
 
 -- Volume level
-local volwidget = wibox.widget.textbox()
+config.widgets.volume.widget = wibox.widget.textbox()
 vicious.register(
-    volwidget,
+    config.widgets.volume.widget,
     vicious.widgets.volume,
     function ( widget, args )
         local result = ""
@@ -138,102 +181,78 @@ vicious.register(
         else
             result = args[1] .. "%"
         end
-        result = '<span foreground="' .. beautiful.widget_alternative_fg .. '" ' .. 
-            'background="' .. beautiful.widget_alternative_bg .. '">' .. 
-            result .. '</span>'
         return result
     end,
-    17,
-    "Master"
+    config.widgets.volume.update_time,
+    config.widgets.volume.device
 )
-config.widget.vol_widget = volwidget
-volwidget:buttons(
+config.widgets.volume.widget:buttons(
     awful.util.table.join(
-        awful.button({ }, 1, volume.mixer), -- Right click
+        awful.button({ }, 1, volume.mixer), -- Normal click
         awful.button({ }, 3, volume.toggle), -- Right click
         awful.button({ }, 4, volume.increase), -- Scroll up
         awful.button({ }, 5, volume.decrease) -- Scroll down
     )
 )
 
-
 -- Systray (nothing fanzy)
-local systray = wibox.widget.systray()
+config.widgets.systray.widget = wibox.widget.systray()
 
-
+-- --------------------------------------------------------------------------
 -- Per screen widgets
-local promptbox = {}
-local layoutbox = {}
-local taglist   = {}
-taglist.buttons = awful.util.table.join(
-    awful.button({ }, 1, awful.tag.viewonly),
-    awful.button({ modkey }, 1, awful.client.movetotag),
-    awful.button({ }, 3, awful.tag.viewtoggle))
+-- --------------------------------------------------------------------------
 
-config.taglist = taglist
-config.promptbox = promptbox
-
--- Wibox initialisation (one per screen)
-local wibox_top = {}
-
--- Add widgets to wiboxes
 for s = 1, screen.count() do
 
-    -- This function return the widget if it must be displayed on this screen
-    -- nil otherwise
+    -- This function add the widget what to the layout if it's on screen where or
+    -- on all screen (nil)
     local add_widget = function(what, layout, where)
         if where == nil or where == s then
             layout:add(what)
         end
     end
 
-    promptbox[s] = awful.widget.prompt()
-    layoutbox[s] = awful.widget.layoutbox(s)
-    taglist[s] = awful.widget.taglist.new(
-        s,
-        awful.widget.taglist.filter.all,
-        taglist.buttons
-    )
-
     -- Create the wibox
-    wibox_top[s] = awful.wibox({
+    -- TODO : iterate over config.widgets.wiboxes to build every desired wiboxes
+    config.widgets.wiboxes.top.widget[s] = awful.wibox({
         screen = s,
         position = "top",
-        height = 18,
+        height = config.widgets.wiboxes.top.size,
+        bg = config.widgets.wiboxes.top.bg
     })
+
+    -- Create widgets 
+    config.widgets.promptbox.widget[s] = awful.widget.prompt()
+    config.widgets.layoutbox.widget[s] = awful.widget.layoutbox(s)
+    config.widgets.taglist.widget[s] = awful.widget.taglist.new(
+        s,
+        awful.widget.taglist.filter.all,
+        config.widgets.taglist.buttons
+    )
     
     -- Add widgets to the wibox
     -- Widgets that are aligned to the left
     local left_group = wibox.layout.fixed.horizontal()
-    left_group:add(layoutbox[s])
-    left_group:add(sep_top_bottom)
-    left_group:add(wibox.widget.background(taglist[s], beautiful.widget_alternative_bg))
-    left_group:add(sep_bottom_top)
-    left_group:add(promptbox[s])
+    add_widget(config.widgets.layoutbox.widget[s], left_group)
+    add_widget(config.widgets.taglist.widget[s], left_group)
+    add_widget(config.widgets.promptbox.widget[s], left_group)
 
     -- Widgets that are aligned to the right
     local right_group = wibox.layout.fixed.horizontal()
-    add_widget(sep_top_bottom, right_group, 1)
-    add_widget(wibox.widget.background(systray, beautiful.widget_alternative_bg), right_group, 1)
-    add_widget(sep_bottom_top, right_group, 1)
-
-    add_widget(batwidget.widget, right_group, 1)
-
-    add_widget(sep_top_bottom, right_group, 1)
-    add_widget(wibox.widget.background(volwidget, beautiful.widget_alternative_bg), right_group, 1)
-    add_widget(sep_bottom_top, right_group, 1)
-
-    right_group:add(datewidget)
+    add_widget(config.widgets.systray.widget, right_group, 1) -- Only on first screen
+    add_widget(config.widgets.battery.widget, right_group, 1) -- Only on first screen
+    add_widget(config.widgets.volume.widget, right_group, 1) -- Only on first screen
+    add_widget(config.widgets.date_time.widget, right_group) -- On all screens
 
     -- Now bring it all together
     local main_group = wibox.layout.align.horizontal()
     main_group:set_left(left_group)
     main_group:set_right(right_group)
 
-    wibox_top[s]:set_widget(main_group)
+    config.widgets.wiboxes.top.widget[s]:set_widget(main_group)
 end
 
 -- Activate shifty now !
 local shifty = require("shifty")
-shifty.taglist = taglist
+shifty.taglist = config.widgets.taglist.widget
 shifty.init()
