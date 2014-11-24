f8662a0ed711170e179e72bfa4de802f23621fdb
Alexis BRENON <brenon.alexis@gmail.com>
2014-11-24 17:43:22 +0100

    Change images used in taglist

--- theme/theme.lua
+++ theme/theme.lua
@@ -21,23 +21,32 @@ theme.fg_focus      = "#E6DB74" -- Monokai yellow
 theme.fg_urgent     = "#F92672" -- Monokai red
 
 theme.border_width  = 2
-theme.border_normal = "#3F3F3F" -- Dark grey
-theme.border_focus  = "#6F6F6F" -- Light grey
-theme.border_marked = "#93B0CD" -- Very light blue
+theme.border_normal = theme.bg_normal
+theme.border_focus  = theme.bg_normal
+theme.border_marked = theme.fg_focus
 
 -- Systray
 theme.bg_systray = "#00000000"
 theme.systray_icon_spacing = 2
 
 -- Taglist
-theme.taglist_bg_focus = "#00000000"
-theme.taglist_bg_occupied = "#00000000"
-theme.taglist_bg_empty = "#00000000"
-theme.taglist_fg_occupied = theme.fg_normal
+theme.taglist_bg_empty = "#00000000" -- No background
+theme.taglist_bg_occupied = theme.taglist_bg_empty
+theme.taglist_bg_focus = theme.taglist_bg_empty
+theme.taglist_bg_urgent = theme.taglist_bg_empty
+
 theme.taglist_fg_empty = theme.fg_normal
+theme.taglist_fg_occupied = theme.fg_normal
+theme.taglist_fg_focus = theme.fg_focus
+theme.taglist_fg_urgent = theme.fg_urgent
+
 theme.taglist_font = "FontAwesome 12"
-theme.taglist_squares_sel   = icons .. "/taglist/squarefw.png"
-theme.taglist_squares_unsel = icons .. "/taglist/squarew.png"
+
+theme.taglist_squares_sel = icons .. "/taglist/none.png"
+theme.taglist_squares_unsel = icons .. "/taglist/none.png"
+theme.taglist_squares_sel_empty = icons .. "/taglist/none.png"
+theme.taglist_squares_unsel_empty = icons .. "/taglist/none.png"
+theme.taglist_squares_resize = "false"
 
 -- Layout icons
 for _, l in pairs(awful.layout.layouts) do
