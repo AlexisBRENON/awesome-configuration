7a1ebd3211ae7d14f3cfda040eff7078c941fbda
Alexis BRENON <brenon.alexis@gmail.com>
2014-12-17 18:20:47 +0100

    Add mouse behaviour definition

---
+++ config/mouse.lua
@@ -0,0 +1,12 @@
+local mouse = {}
+
+mouse.global = {}
+
+mouse.client = {
+    {{        }, 1, client_wrapper.focus, nil, "Give focus to window"},
+    {{ modkey }, 1, client_wrapper.move, nil, "Move window in floating layout"},
+    {{ modkey }, 3, client_wrapper.resize, nil, "Resize window"}
+}
+
+return mouse
+
