5d3807e7619e17973efaece76aa02480930e89d4
Alexis BRENON <brenon.alexis@gmail.com>
2014-12-17 18:17:39 +0100

    Add the 'main' configuration file It contains informations related to the whole awesome config

---
+++ config/main.lua
@@ -0,0 +1,12 @@
+local awful_wrapper = require('backend/awful_wrapper')
+
+local main = {
+    modkey = 'Mod4',
+    config = awful_wrapper.getdir('config'),
+    cache = awful_wrapper.getdir('cache'),
+    hostname = awful_wrapper.get_hostname(),
+    theme = awful_wrapper.getdir('config') .. "/theme/theme.lua",
+}
+
+return main
+
