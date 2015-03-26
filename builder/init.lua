c54d11dcffa751f82a74cf5d0ec6c8673c454e48
Alexis BRENON <brenon.alexis@gmail.com>
2015-03-26 10:44:36 +0100

    Enable autofocus

--- builder/init.lua
+++ builder/init.lua
@@ -1,6 +1,7 @@
 local builder = {}
 
 function builder.build(config)
+    require('awful/autofocus')
     require('builder/appearance').build(config.theme)
     require('builder/tags').build(config.tags)
     require('builder/rules').build(config.rules)
