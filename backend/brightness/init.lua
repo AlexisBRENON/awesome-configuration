e95c7e4c62d955612def5e4f8479395d72e11927
Alexis BRENON <brenon.alexis@gmail.com>
2015-04-01 18:29:43 +0200

    New backlight support implementation This allow to easily add other backends than xbacklight

---
+++ backend/brightness/init.lua
@@ -0,0 +1,60 @@
+-- Handle brightness
+local naughty = require("naughty")
+local beautiful = require("beautiful")
+local log = require("utils/log")
+local submodule = require("utils/submodule")
+
+local brightness = {}
+
+local import_name = ...
+local notification_id = nil
+local _increase = nil
+local _decrease = nil
+
+local function notify()
+    local backlight_percent = _get()
+    notification_id = naughty.notify({
+        text = backlight_percent .. "%",
+        icon = beautiful.icons .. "/brightness/" .. backlight_percent .. ".png",
+        icon_size = 22,
+        replaces_id = notification_id
+    }).id
+end
+
+local function init()
+    -- Search for a supported backend
+    for _, backend_import_name in ipairs(submodule.fetch_submodules(import_name)) do
+        local backend = require(backend_import_name)
+        if backend.is_supported() then
+            log.debug('backlight support : ON (' .. backend.name ..')')
+            _increase = backend.increase
+            _decrease = backend.decrease
+            _get = backend.get
+            _set = backend.set
+        end
+    end
+    -- If not found assign a warning function
+    if _get == nil then
+        log.info('backlight support : OFF')
+        local function fallback ()
+            log.warning('backlight not supported...')
+        end
+        _increase = fallback
+        _decrease = fallback
+        _get = fallback
+        _set = fallback
+    end
+end
+
+function brightness.increase()
+    _increase()
+    notify()
+end
+
+function brightness.decrease()
+    _decrease()
+    notify()
+end
+
+init()
+return brightness
---
+++ backend/brightness/xbacklight.lua
@@ -0,0 +1,48 @@
+local module = {}
+
+local function get()
+    local current_backlight
+    local f = io.popen("xbacklight -get")
+    if f then
+        current_backlight = math.floor(tonumber(f:read("*all")))
+        f:close()
+    end
+    return current_backlight
+end
+
+local function increase(percent)
+    if not percent then
+        percent = 10
+    end
+    os.execute('xbacklight -inc ' .. percent)
+end
+
+local function decrease(percent)
+    if not percent then
+        percent = 10
+    end
+    os.execute('xbacklight -dec ' .. percent)
+end
+
+local function set(percent)
+    if not percent then
+        error('No value given to the backlight')
+    end
+    os.execute('xbacklight -set ' .. percent)
+end
+
+local function is_supported()
+    return (get() ~= nil)
+end
+
+module = {
+    name = "xbacklight",
+    is_supported = is_supported,
+    get = get,
+    increase = increase,
+    decrease = decrease,
+    set = set,
+}
+
+return module
+
