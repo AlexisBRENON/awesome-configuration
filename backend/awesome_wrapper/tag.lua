17744d2ed80a1d2cf363832a2bd26a13b8ede74f
Alexis BRENON <brenon.alexis@gmail.com>
2015-06-05 16:08:17 +0200

    Add 'move_tag' function to move a complete tag accross screens

--- backend/awesome_wrapper/tag.lua
+++ backend/awesome_wrapper/tag.lua
@@ -26,4 +26,49 @@ function tag_wrapper.change_layout(relative_idx)
     awful.layout.inc(relative_idx, mouse.screen, awful.layout.layouts)
 end
 
+local function tag2index(screen, tag)
+    for i, t in ipairs(awful.tag.gettags(screen)) do
+        if t == tag then return i end
+    end
+end
+
+function tag_wrapper.move_tag(relative_index)
+    local current_screen = mouse.screen
+    local current_screen_index = current_screen - 1
+    local current_tag = awful.tag.selected(current_screen)
+    local current_tag_index = tag2index(current_screen, current_tag)
+
+    local new_screen = current_screen
+    local new_screen_index = current_screen_index
+    local new_tag_index = current_tag_index + relative_index
+
+    -- Compute the new screen of the tag
+    if new_tag_index < 1 then
+        new_screen_index = current_screen_index - 1
+    elseif new_tag_index > #awful.tag.gettags(current_screen) then
+        new_screen_index = current_screen_index + 1
+    end
+    new_screen_index = new_screen_index % screen.count()
+    new_screen = new_screen_index + 1
+
+    -- Define new tag index depending on already existing tag on new screen
+    if new_tag_index < 1 then
+            new_tag_index = #awful.tag.gettags(new_screen)
+        if new_screen ~= current_screen then
+            new_tag_index = new_tag_index + 1
+        end
+    elseif new_tag_index > #awful.tag.gettags(current_screen) then
+        new_tag_index = 1
+    end
+
+    -- Move tag to the new screen
+    if new_screen ~= current_screen then
+        awful.tag.setscreen(current_tag, new_screen)
+        awful.tag.viewonly(current_tag)
+        mouse.screen = new_screen
+    end
+    -- Move tag to the new index
+    awful.tag.move(new_tag_index, current_tag)
+end
+
 return tag_wrapper
